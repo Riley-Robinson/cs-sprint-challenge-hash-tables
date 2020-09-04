c1d790450bfc88a5a9dea84b324fe5c5a6705465
Riley <rrtoinfinity@gmail.com>
2020-09-04 11:14:47 -0400

    ex1 done adding documentation

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -4,4 +4,21 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     # Your code here
 
+    data = {}
+
+    for i in range(len(weights)):
+
+
+        data[weights[i]] = i
+
+    for i in range(len(weights)):
+
+
+        limitweight = limit-weights[i]
+
+        if limitweight in data:
+
+            return(max(i, data[limitweight]), min(i, data[limitweight]))
+ 
+ 
     return None
