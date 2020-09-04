1d01843546bb2f20406a069d3b7ec38c14799dc4
Riley <rrtoinfinity@gmail.com>
2020-09-04 11:27:01 -0400

    documentation for ex1 as well as ex2 and documenttation for ex2

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,22 +2,26 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
+    #  UPER : Need a function to find 2 items with a sum weight = to the weight limit.  Must return a tuple of numbers (zero, one)
     # Your code here
 
     data = {}
 
     for i in range(len(weights)):
 
-
+        # loop through
         data[weights[i]] = i
 
+    #set the weight to a key
     for i in range(len(weights)):
-
-
+    
+        # loop fir the range
         limitweight = limit-weights[i]
 
+        #calculate the dif
         if limitweight in data:
 
+            #if weight limit is in the data,
             return(max(i, data[limitweight]), min(i, data[limitweight]))
  
  
